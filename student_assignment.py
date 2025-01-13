309bc58a164c463d6c84430e09f2c57b27b63c1e
r17652001 <r17652001@gmail.com>
2025-01-13 16:50:08 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -7,6 +7,7 @@ from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
+from rich import print as ppprint
 from langchain_core.output_parsers import JsonOutputParser
 
 gpt_chat_version = 'gpt-4o'
