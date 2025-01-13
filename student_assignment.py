6384768505aa2a6f0793e5e8407eae80b90cb7b6
r17652001 <r17652001@gmail.com>
2025-01-13 15:22:29 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -1,41 +1,69 @@
 import json
-import pprint
+import requests
 import traceback
-from urllib import response
 
 from model_configurations import get_model_configuration
 
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
-from rich import print as ppprint
-from langchain_core.output_parsers import JsonOutputParser
+from langchain.chains import LLMChain
+from langchain.tools import Tool
+from langchain.agents import AgentExecutor, create_openai_functions_agent
 
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+import base64
+from mimetypes import guess_type
+from langchain_core.messages import HumanMessage, SystemMessage
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
-json_parser = JsonOutputParser()
-format_instructions = json_parser.get_format_instructions()
 
 def generate_hw01(question):
     llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature']
-    )
-    message = HumanMessage(
-            content=[
-                {"type": "text", "text": question},
-            ]
+        model = gpt_config['model_name'],
+        deployment_name = gpt_config['deployment_name'],
+        openai_api_key = gpt_config['api_key'],
+        openai_api_version = gpt_config['api_version'],
+        azure_endpoint = gpt_config['api_base'],
+        temperature= gpt_config['temperature']
     )
 
-    response = llm.invoke(question+"請用json的格式輸出, 請附\"date\" and \"name\"的標籤, 放在Result的攔位裡, 只顯示包含Result之後的攔位內容就好,\
-    不要顯示'''json字串"
-                         f"{format_instructions}, 使用台灣語言")
-    #print(response.content)
-    return response.content
+    system = """
+            You are a helpful assistant.
+            Please respond in JSON format.
+            The top-level key must be 'Result', and its value must be a list of objects.
+            Each object should contain two keys: 'date' (the date of the holiday) and 'name' (the name of the holiday).
+            """
+    try:
+        answer_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system),
+                ("human", "Answer the question based on the below description and only use English or traditional Chinese. Please respond in JSON format."),
+                ("human", f"Question: {question} \n "),
+            ]
+        )
+
+        json_llm = llm.bind(response_format={"type": "json_object"})
+        
+        rag_chain = LLMChain(
+            llm=json_llm,
+            prompt=answer_prompt,
+            output_parser=StrOutputParser()
+        )
+
+        # RAG generation
+        answer = rag_chain.invoke({"question": question})
+        print(answer)
+        text_content = answer.get('text')
+
+        return text_content
+    except Exception as e:
+        traceback_info = traceback.format_exc()
+        print(traceback_info)
     
 def generate_hw02(question):
     pass
